e5183983ede30c23e622bcf7df54a9d420ed2dbc
CarlosaAOL <166845362+CarlosaAOL@users.noreply.github.com>
2024-05-02 18:01:55 -0300

    aprendendo

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 90
+console.log(`a velocidade do seu carro e ${vel}Km/h`)
+if(vel > 60){ // condição simples!
+    console.log(`Você ultaprassou a velocidae permitida. MULTADO!`)
+}
+console.log(`Dirija usando sempre o cinto de segurança`)
---
+++ exercicios/aula011/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'JAPÃO'
+console.log(`Vivendo em ${pais} `)
+if(pais != 'JAPÃO'){
+    console.log(`Você e Estrangeiro(a)!`)
+} else{
+    console.log(`Você e Japones(a)!`)
+}
+    
