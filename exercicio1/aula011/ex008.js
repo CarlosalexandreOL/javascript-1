2171d9ad7ed6d55bb21b89806644f051804f0e7d
CarlosOVL <151205009+CarlosalexandreOL@users.noreply.github.com>
2024-05-17 14:22:25 -0300

    ex feitos

---
+++ exercicio1/aula011/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 60
+console.log(`a velocidade do seu carro e de ${velocidade}km/h`)
+if(velocidade > 60){ //Condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!!')
+}
+console.log('Dirija sempre usando cinto se segurança.')
---
+++ exercicio1/aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'japão'
+console.log(`Vivendo em ${pais}`)
+if(pais != 'brasil'){
+    console.log('Você e brasileiro')
+}else{
+    console.log('Você e estrangeiro')
+}
